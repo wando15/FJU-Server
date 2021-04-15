42221acdc31f2a4a9f6edf1ddd264a827d46655d
w.cesario.de.sousa <38380430+wando15@users.noreply.github.com>
2021-04-14 22:38:27 -0300

    Revert "Delete config directory"
    This reverts commit 40c00cf58f6f53aa9451c637efa53be0b38eb404.

---
+++ config/db-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    database: "fjudb",
+    username: "root",
+    password: "Changeme@@09",
+    host: "localhost",
+    port: "3306",
+    dialect: "mysql",
+    define: {
+        timestamps: true,
+        underscored: true
+    },
+    logging: false
+}
